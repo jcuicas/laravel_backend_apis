cab00859d5fa7721092a8520a5ace82f525121e1
jcuicas <josecuicas@gmail.com>
2024-12-06 21:28:03 -0400

    primer commit

---
+++ app/Models/Categoria.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Producto;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Categoria
+ *
+ * @property $id
+ * @property $categoria
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @property Producto[] $productos
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Categoria extends Model
+{
+    
+    protected $perPage = 20;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ['categoria'];
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function productos()
+    {
+        return $this->hasMany(Producto::class, 'id', 'idcategoria');
+    }
+    
+}
---
+++ app/Models/Producto.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Categoria;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Producto
+ *
+ * @property $id
+ * @property $codigo
+ * @property $producto
+ * @property $idcategoria
+ * @property $existencia
+ *
+ * @property Categoria $categoria
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Producto extends Model
+{
+    
+    protected $perPage = 20;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ['codigo', 'producto', 'idcategoria', 'existencia'];
+
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function categoria()
+    {
+        return $this->belongsTo(Categoria::class, 'idcategoria', 'id');
+    }
+    
+}
